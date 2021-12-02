b9bc156e331f316c9c51716f3cc22b821f53753a
cdmac94 <collinmacleod79@gmail.com>
2021-12-02 01:53:40 +0000

    Sending messages now

--- input.js
+++ input.js
@@ -1,5 +1,7 @@
+let connection;
+
 const setupInput = function (conn) {
-  connection = conn
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,23 +11,36 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = (key) => {
-  if (key === 'o'){
-    process.exit()
+  if (key === '\u0003'){
+    process.exit();
   }
   if (key === 'w'){
-    connection.write("Move: up")
+    connection.write("Move: up");
   }
   if (key === 's'){
-    connection.write('Move: down')
+    connection.write('Move: down');
   }
   if (key === 'a'){
-    connection.write('Move: left')
+    connection.write('Move: left');
   }
   if (key === 'd'){
-    connection.write('Move: right')
+    connection.write('Move: right');
+  }
+  if (key === '1'){
+    connection.write('Say: >:)');
+    console.log("It also works");
+  }
+  if (key === '2'){
+    connection.write(`Say: I'm gunnnnaaaa crasssshhhh`);
+  }
+  if (key === '3'){
+    connection.write('Say: Can anybody here me anymore?');
+  }
+  if (key === '4'){
+    connection.write(`Say: right... of course you can't`);
   }
 }
 
 console.log("Connecting ...");
 
-module.exports = { setupInput }
+module.exports = { setupInput };
